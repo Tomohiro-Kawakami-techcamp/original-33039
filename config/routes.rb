ec0176d2cd7924860b7df44dee2fc417f3f18085
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-04-19 11:44:10 +0900

    これ消さないと起動しない

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin
-  get "/admin" => "admin/posts#index"
-  namespace :admin do
-    resources :posts
-  end
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index, :new, :create]
